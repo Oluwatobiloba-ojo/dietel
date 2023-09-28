9ad6d9727525a958e1f1bca7cc65fe602ee0123c
Ojo Oluwatobi <ojot630@gmail.com>
2023-09-28 11:16:18 +0100

    initial commit

--- src/Snacks/Arrays.java
+++ src/Snacks/Arrays.java
@@ -1,2 +1,23 @@
-package Snacks;public class Arrays {
+package Snacks;
+
+import java.util.Scanner;
+
+public class Arrays{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int [] array = new int[10];
+        for (int number = 0; number < array.length; number++){
+            System.out.println("Enter a number: ");
+            int score = input.nextInt();
+            array[number] = score;
+        }
+        System.out.println(java.util.Arrays.toString(array));
+        int largest = 0;
+        for (int numbers = 0; numbers < array.length ; numbers ++) {
+            if (array[numbers] > largest){
+                largest = array[numbers];
+            }
+        }
+        System.out.println(largest);
+    }
 }
