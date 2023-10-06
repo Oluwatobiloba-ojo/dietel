23b1798e2c90a53b5fb7c2a4b887c5627b6918bf
Ojo Oluwatobi <ojot630@gmail.com>
2023-10-06 03:18:29 +0100

    commit

---
+++ src/arraysnacks/SieveOfEratosthenes.java
@@ -0,0 +1,29 @@
+package arraysnacks;
+
+import java.util.Arrays;
+
+public class SieveOfEratosthenes {
+
+
+    public static int checkPrimeIndices(boolean[] prime) {
+        for (int index = 2; index < prime.length; index++) {
+            for (int multiple = index + index; multiple < prime.length ; multiple+= index) {
+                prime[multiple] = false;
+            }
+        }
+        int length;
+        length = calculateNumbersOfThoseTrue(prime);
+        return length;
+    }
+
+
+    private static int calculateNumbersOfThoseTrue(boolean[] prime) {
+        int length = 0;
+        for (boolean b : prime) {
+            if (b) {
+                length++;
+            }
+        }
+        return length;
+    }
+}
---
+++ test/arraysnacks/SieveOfEratosthenesTest.java
@@ -0,0 +1,38 @@
+package arraysnacks;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SieveOfEratosthenesTest {
+
+    @Test
+    void testForAPrimeNumberInAnArrayOfIndex(){
+        boolean [] prime = new boolean[100];
+        for (int i = 2; i < prime.length ; i++) {
+            prime[i] = true;
+        }
+        int primeNumber =  SieveOfEratosthenes.checkPrimeIndices(prime);
+        int answer = 25;
+        assertEquals(answer, primeNumber);
+    }
+
+    @Test
+    void testForAPrimeNUmberInAnArrayOfIndices2(){
+        boolean [] index = {false, false, true, true, true, true, true, true, true, true};
+        int answer = 4;
+        int primeNumber = SieveOfEratosthenes.checkPrimeIndices(index);
+        assertEquals(primeNumber, answer);
+    }
+
+    @Test
+    void testForThePrimeNumberUsingSieveOfEratosthenes(){
+        boolean [] index = {false, false, true, true, true, true,true,true,true};
+        int answer = 4;
+        int primeNumber = SieveOfEratosthenes.checkPrimeIndices(index);
+        assertEquals(answer, primeNumber);
+    }
+
+}
