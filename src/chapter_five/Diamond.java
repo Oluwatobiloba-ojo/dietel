d0a07b85be480d0830d27045419347a58c0eb755
Ojo Oluwatobi <ojot630@gmail.com>
2023-10-17 05:57:39 +0100

    commits

--- src/chapter_five/Diamond.java
+++ src/chapter_five/Diamond.java
@@ -3,7 +3,7 @@ package chapter_five;
 public class Diamond {
     public static void main(String[] args) {
         int number = 19;
-        for (int index = 1; index <= number ; index += 2) {
+        for (int index = 1; index < number ; index += 2) {
             System.out.println();
             for (int count = index; count <= number; count++) {
                 System.out.print("  ");
@@ -15,7 +15,7 @@ public class Diamond {
                 System.out.print("* ");
             }
         }
-        for (int row = 1; row < number; row+= 2) {
+        for (int row = 1; row <= number; row+= 2) {
             System.out.println();
             for (int index = 1; index <= row + 1; index++) {
                 System.out.print("  ");
@@ -23,7 +23,7 @@ public class Diamond {
             for (int count = row + 1; count < number; count++) {
                 System.out.print("* ");
             }
-            for (int column = row; column < number; column++) {
+            for (int column = row; column <= number; column++) {
                 System.out.print("* ");
             }
         }
