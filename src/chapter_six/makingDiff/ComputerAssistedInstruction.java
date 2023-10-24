d9f21d89b75e9efbf8a2cb1ff4cc63645945b408
Ojo Oluwatobi <ojot630@gmail.com>
2023-10-24 10:46:16 +0100

    commits

--- src/chapter_six/makingDiff/ComputerAssistedInstruction.java
+++ src/chapter_six/makingDiff/ComputerAssistedInstruction.java
@@ -2,27 +2,41 @@ package chapter_six.makingDiff;
 
 import java.security.SecureRandom;
 import java.util.Scanner;
-
 public class ComputerAssistedInstruction {
+    static SecureRandom myRandom = new SecureRandom();
+   static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        SecureRandom myRandom = new SecureRandom();
         String stopper = "CONTINUE";
         while (stopper.equals("CONTINUE")){
-            int firstNumber = 1 + myRandom.nextInt(10);
-            int secondNumber = 1 + myRandom.nextInt(10);
-            int guess = 1 + myRandom.nextInt(3);
-            System.out.printf("How much is %d times %d\n", firstNumber, secondNumber);
-            int result = input.nextInt();
-            int product = firstNumber * secondNumber;
-            while (result != product){
-                System.out.println("No please Try Again");
-                System.out.printf("How much is %d times %d\n", firstNumber, secondNumber);
-                result = input.nextInt();
-            }
+            computer();
             System.out.println("Very Good");
             System.out.println("Enter stop to end the program and enter continue to move on ");
-            stopper = input.next();
+            stopper = input.next().toUpperCase();
         }
+    }
+
+
+
+
+
+
+
+    public static void computer(){
+        int firstNumber = 1 + myRandom.nextInt(10);
+        int secondNumber = 1 + myRandom.nextInt(10);
+        System.out.printf("How much is %d times %d\n", firstNumber, secondNumber);
+        int product = input.nextInt();
+        int result = multiplication(firstNumber, secondNumber);
+        repeatQuestion(result, firstNumber, secondNumber, product);
+    }
+    public static void repeatQuestion(int result, int first, int second, int product){
+        while (result != product){
+            System.out.println("No please Try Again");
+            System.out.printf("How much is %d times %d\n", first, second);
+            product = input.nextInt();
+        }
+    }
+    public static int multiplication(int first, int second){
+        return first * second;
     }
 }
