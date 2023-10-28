754fe0a2e2478412bb03a3aec5425f8069ef8d3a
Ojo Oluwatobi <ojot630@gmail.com>
2023-10-28 05:13:41 +0100

    commits

--- src/chibuzorarrayexcercise/StudentGrade.java
+++ src/chibuzorarrayexcercise/StudentGrade.java
@@ -3,7 +3,7 @@ package chibuzorarrayexcercise;
 import static chibuzorarrayexcercise.StudentGradeFunction.*;
 
 public class StudentGrade {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         int student = numberOfStudent();
         int subject = numberOfSubject();
         int [][] grades = new int[student][subject];
