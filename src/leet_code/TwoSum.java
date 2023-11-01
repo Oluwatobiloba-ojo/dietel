da2ab5f8fc7efbc740b8cd08db16232b26ae974f
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-01 22:06:44 +0100

    commits

--- src/leet_code/TwoSum.java
+++ src/leet_code/TwoSum.java
@@ -3,19 +3,13 @@ package leet_code;
 public class TwoSum {
 
     public static int[] twoSum(int[] numbers, int target){
-        int[] indexesOfSumOfTarget = new int[2];
-        int stopper = 0;
         for (int firstSum = 0; firstSum < numbers.length;firstSum++){
             for (int secondSum = firstSum + 1; secondSum < numbers.length; secondSum++){
                 if (target - numbers[secondSum] == numbers[firstSum]){
-                    indexesOfSumOfTarget[0] = firstSum;
-                    indexesOfSumOfTarget[1] = secondSum;
-                    stopper = -1;
-                    break;
+                    return new int[]{firstSum, secondSum};
                 }
             }
-            if (stopper == -1) break;
         }
-        return indexesOfSumOfTarget;
+        return null;
     }
 }
