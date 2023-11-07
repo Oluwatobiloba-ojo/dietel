9deb82c76f3487c0660fe04e4af53b92be46efec
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-07 22:04:54 +0100

    commits

--- test/CalculatorTest.java
+++ test/CalculatorTest.java
@@ -1,9 +1,24 @@
-package calculator;
-
+import calculator.Calculator;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
     @Test
-    public void testForThe
+    public void testForTheSumUpOfThreeDifferentNumbers(){
+        Calculator myCalculator = new Calculator();
+        assertEquals(myCalculator.calculate("2+3+5"), 10);
+    }
+    @Test
+    public void testForTheProductOfThreeDifferentNumbers(){
+        Calculator myCalculator = new Calculator();
+        assertEquals(myCalculator.calculate("2*3*5"), 30);
+    }
+
+    @Test
+    public void testForTheAdditionalArithmeticOperationOnAnExpression(){
+        Calculator myCalculator = new Calculator();
+        assertEquals(myCalculator.calculate("2+3-4+5*6/2"), 16);
+    }
 }
