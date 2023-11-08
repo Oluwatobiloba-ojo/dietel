391cc855900610b53a8108d027c801d8002304ca
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-08 22:15:43 +0100

    commits

--- test/leet_code/SubSequenceTest.java
+++ test/leet_code/SubSequenceTest.java
@@ -1,4 +1,30 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SubSequenceTest {
-  
+
+    @Test
+    public void testThatASubSequenceOfAnArrayInTheSameRange(){
+        int [] subArray = {9, 1, 7};
+        int[] arrayNumbers = {4, 9, -8, 6, 1, 7, 6};
+        assertTrue(SubSequence.isValidSubsequence(subArray, arrayNumbers));
+    }
+
+    @Test
+    public void testForASubSequenceOfAnArrayHavingTheSameOrderOfTheArray(){
+        int [] subArray = {3, -8, 1};
+        int[] arrayNumbers = {5, 2, 1, 3, -2, -8, 1};
+        assertTrue(SubSequence.isValidSubsequence(subArray, arrayNumbers));
+    }
+
+    @Test
+    public void testForASubSequenceOfAnArrayHavingTheSameOrderInTheSuperArray(){
+        int [] subArray = {3, -8, 5};
+        int[] arrayNumbers = {5, 2, 1, 3, -2, -8, 1};
+        assertFalse(SubSequence.isValidSubsequence(subArray, arrayNumbers));
+    }
+
 }
