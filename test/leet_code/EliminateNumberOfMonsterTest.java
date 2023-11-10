5274d0eb28b4e1e8fb2fcc89ca7f89163041aec1
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-10 22:01:57 +0100

    commits

--- test/leet_code/EliminateNumberOfMonsterTest.java
+++ test/leet_code/EliminateNumberOfMonsterTest.java
@@ -1,4 +1,36 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class EliminateNumberOfMonsterTest {
-  
+
+    @Test
+    public void testForThatTheEliminationOfMonsterBasedOnTheSpeedOfWeapon(){
+        int[] distanceOfMonster = {1, 3, 4};
+        int[] speed = {1, 1, 1};
+        assertEquals(3, EliminateNumberOfMonster.eliminateMaximum(distanceOfMonster, speed));
+    }
+
+    @Test
+    public void testForTheEliminationOfMaximumDistanceOfMonster(){
+        int[] distanceOfMonster = {1, 1, 2, 3};
+        int[] speed = {1, 1, 1, 1};
+        assertEquals(1, EliminateNumberOfMonster.eliminateMaximum(distanceOfMonster, speed));
+    }
+
+    @Test
+    public void testForTheEliminationOfMaximumDistanceOf(){
+        int[] distanceOfMonster = {3, 2, 4};
+        int[] speed = {5, 3, 2};
+        assertEquals(1, EliminateNumberOfMonster.eliminateMaximum(distanceOfMonster, speed));
+    }
+    @Test
+    public void testForTheEliminationOfMaximumDistanceOfWithDifferentSpeed(){
+        int[] distanceOfMonster = {3,5,7,4,5};
+        int[] speed = {2,3,6,3,2};
+        assertEquals(2, EliminateNumberOfMonster.eliminateMaximum(distanceOfMonster, speed));
+    }
+
 }
