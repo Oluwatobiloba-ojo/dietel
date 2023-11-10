d2b1446081e1d30302110eba488cd55303cee725
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-10 21:41:16 +0100

    commits

--- test/leet_code/FibonacciTest.java
+++ test/leet_code/FibonacciTest.java
@@ -1,2 +1,23 @@
-package leet_code;public class FibonacciTest {
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FibonacciTest {
+
+    @Test
+    public void testForTheFibonacciTotalOfALengthOfNumbers(){
+        assertEquals(Fibonaci.fibonacci(2), 1);
+    }
+
+    @Test
+    public void testForTheFibonacciTotalOfALengthOfFifteen(){
+        assertEquals(Fibonaci.fibonacci(15), 377);
+    }
+
+    @Test
+    public void testForTheFibonacciTotalOfALengthOfFive(){
+        assertEquals(Fibonaci.fibonacci(5), 3);
+    }
 }
