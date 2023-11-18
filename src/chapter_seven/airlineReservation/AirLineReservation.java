39afe6c7c1595f4a79ebc6497c04e72eb46fbe1c
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-18 20:51:47 +0100

    commit

--- src/chapter_seven/airlineReservation/AirLineReservation.java
+++ src/chapter_seven/airlineReservation/AirLineReservation.java
@@ -1,19 +1,23 @@
-package chapter_seven;
+package chapter_seven.airlineReservation;
 
 import java.security.SecureRandom;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class AirLineReservation {
 
     private final boolean[] airplaneSeat = new boolean[10];
 
     private String seatNumber;
-    private int numberOfFirstClass = 0;
+    private int numberOfFirstClass = 5;
+
+    private int numberOfEconomy = 10;
 
     private int position;
 
     public AirLineReservation(int length){
         for (int seat = 0; seat < length; seat++){
-            seatNumber += String.valueOf(seat)+ ",";
+            seatNumber += seat + ",";
         }
     }
 
@@ -29,11 +33,10 @@ public class AirLineReservation {
         }else {
             throw new IllegalArgumentException("Invalid input");
         }
-
     }
 
     private void economySection() {
-        generateSeatPosition(6, 10);
+        generateSeatPositionForEconomyClass();
         if (!airplaneSeat[position]) {
             airplaneSeat[position] = true;
         }else {
@@ -43,22 +46,34 @@ public class AirLineReservation {
     }
 
     private void firstClassSection() {
-        generateSeatPosition(1, 5);
+        generateSeatPositionForFirstClass();
         if (!airplaneSeat[position]) {
             airplaneSeat[position] = true;
         }else throw new IllegalArgumentException("Section already fulled ");
     }
 
-    private void generateSeatPosition(int startPosition, int stopPosition) {
+    private void generateSeatPositionForFirstClass() {
         SecureRandom random = new SecureRandom();
-        if (numberOfFirstClass != 5) {
-            int number = random.nextInt(startPosition - 1, stopPosition);
+        if (numberOfFirstClass != 0) {
+            int number = random.nextInt(0, 5);
             while (!seatNumber.contains(String.valueOf(number))) {
-                number = random.nextInt(startPosition - 1, stopPosition);
+                number = random.nextInt(0, 5);
             }
             position = number;
+            numberOfFirstClass--;
             seatNumber = seatNumber.replace(String.valueOf(number), "A");
-            numberOfFirstClass++;
+        }
+    }
+    private void generateSeatPositionForEconomyClass() {
+        SecureRandom random = new SecureRandom();
+        if (numberOfEconomy != 5) {
+            int number = random.nextInt(5, 10);
+            while (!seatNumber.contains(String.valueOf(number))) {
+                number = random.nextInt(5, 10);
+            }
+            position = number;
+            numberOfEconomy--;
+            seatNumber = seatNumber.replace(String.valueOf(number), "A");
         }
     }
 
@@ -69,5 +84,22 @@ public class AirLineReservation {
             if (airplaneSeat[seat]) seatOf = seat;
         }
         return seatOf;
+    }
+    public boolean[] getSeat(){
+        return airplaneSeat;
+    }
+
+    void display() {
+        Scanner input = new Scanner(System.in);
+        try{
+            System.out.println("Please type 1 for first class and 2 for Economy Class");
+            String segment = input.nextLine();
+            if (segment.matches("[\\d+]")) {
+                orderForSeatInSeveralSection(Integer.parseInt(segment));
+                System.out.println(Arrays.toString(getSeat()));
+            }else System.out.println("Invalid input");
+        }catch (IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
     }
 }
--- src/chapter_seven/airlineReservation/AirLineReservationMain.java
+++ src/chapter_seven/airlineReservation/AirLineReservationMain.java
@@ -1,2 +1,18 @@
-package chapter_seven.airlineReservation;public class AirLineReservationMain {
+package chapter_seven.airlineReservation;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class AirLineReservationMain {
+   static AirLineReservation airline = new AirLineReservation(10);
+   static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter yes to book a flight and no to stop the booking ");
+        String value = input.nextLine().toLowerCase();
+        while (value.equals("yes")){
+               airline.display();
+            System.out.println("Enter no to stop booking or yes to continue booking: ");
+            value = input.nextLine();
+        }
+    }
 }
