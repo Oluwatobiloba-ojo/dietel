1fb6b2fd479919d90de35567c827ff084fafd8a4
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-18 20:49:08 +0100

    commit

--- test/leet_code/UniqueBinaryStringTest.java
+++ test/leet_code/UniqueBinaryStringTest.java
@@ -2,26 +2,31 @@ package leet_code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class UniqueBinaryStringTest {
 
     @Test
     public void testForTheAStringWhichIsNotFoundInTheStringArray(){
         String[] nums = {"01", "10"};
-        assertEquals("00", UniqueBinaryString.unique(nums));
+        assertEquals("00", UniqueBinaryString.findDifferentBinaryString(nums));
     }
 
     @Test
     public void testForThatAStringWhichIsNotFoundInTheStringArray(){
         String[] nums = {"00", "01"};
-        assertEquals("11", UniqueBinaryString.unique(nums));
+        assertEquals("11", UniqueBinaryString.findDifferentBinaryString(nums));
     }
-
     @Test
     public void testForThatAStringWhichIsNotFoundInTheStringOfArray(){
         String[] nums = {"111","011","001"};
-        assertEquals("000", UniqueBinaryString.unique(nums));
+        assertEquals("000", UniqueBinaryString.findDifferentBinaryString(nums));
+    }
+
+    @Test
+    public void testForThatAString_0WhichIsNotFoundInTheStringOfArray(){
+        String[] nums = {"1"};
+        assertEquals("0", UniqueBinaryString.findDifferentBinaryString(nums));
     }
 
 
