d403ed0dd1c5b5b5d62c76ddcb63e9288bf558c7
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-18 20:41:05 +0100

    commit

--- test/chapter_seven/AirLineReservationTest.java
+++ test/chapter_seven/AirLineReservationTest.java
@@ -1,4 +1,61 @@
+package chapter_seven;
+
+import chapter_seven.airlineReservation.AirLineReservation;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class AirLineReservationTest {
-  
+    AirLineReservation airplane;
+    @BeforeEach
+    public void setAirplane(){
+        airplane = new AirLineReservation(10);
+    }
+
+    @Test
+    public void testThatForTheCapacityOfTheAirplaneSection(){
+        int space = airplane.getSpace();
+        assertEquals(space, 10);
+    }
+
+    @Test
+    public void testForTheOneToBeForTheSeatOfFirstClass(){
+        airplane.orderForSeatInSeveralSection(1);
+        int position = airplane.checkForOrderSeatPosition();
+        boolean seatPosition = position >= 0 && position <= 5;
+        assertTrue(seatPosition);    }
+
+    @Test
+    public void testForTheAirplaneSeatForEconomySection(){
+        airplane.orderForSeatInSeveralSection(2);
+        int position = airplane.checkForOrderSeatPosition();
+        boolean seatPosition = position >= 5 && position < 10;
+        assertTrue(seatPosition);
+    }
+
+    @Test
+    public void testForWhenTheWrongInputForSectionNotAvailable(){
+        assertThrows(IllegalArgumentException.class,()-> airplane.orderForSeatInSeveralSection(5));
+    }
+
+    @Test
+    public void testForWhenTheASectionIsFulledWhatShouldHappen(){
+        airplane.orderForSeatInSeveralSection(1);
+        airplane.orderForSeatInSeveralSection(1);
+        airplane.orderForSeatInSeveralSection(1);
+        airplane.orderForSeatInSeveralSection(1);
+        airplane.orderForSeatInSeveralSection(1);
+        assertThrows(IllegalArgumentException.class, ()-> airplane.orderForSeatInSeveralSection(1));
+    }
+
+    @Test
+    public void testForWhenEconomySectionIsFulled(){
+        airplane.orderForSeatInSeveralSection(2);
+        airplane.orderForSeatInSeveralSection(2);
+        airplane.orderForSeatInSeveralSection(2);
+        airplane.orderForSeatInSeveralSection(2);
+        airplane.orderForSeatInSeveralSection(2);
+        assertThrows(IllegalArgumentException.class, () -> airplane.orderForSeatInSeveralSection(2));
+    }
 }
