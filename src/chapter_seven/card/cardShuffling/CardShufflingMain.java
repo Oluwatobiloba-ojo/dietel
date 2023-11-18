7174e9da678e7d87d88b58efed845cde6ff3341c
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-18 20:40:23 +0100

    commit

--- src/chapter_seven/card/cardShuffling/CardShufflingMain.java
+++ src/chapter_seven/card/cardShuffling/CardShufflingMain.java
@@ -1,2 +1,17 @@
-package chapter_seven.card.cardShuffling;public class CardShufflingMain {
+package chapter_seven.card.cardShuffling;
+
+import chapter_seven.card.Card;
+
+import static chapter_seven.card.cardShuffling.CardShufflingAndDealing.displayCard;
+import static chapter_seven.card.cardShuffling.CardShufflingAndDealing.populateFiveCard;
+
+public class CardShufflingMain {
+    public static void main(String[] args) {
+        Card [] card1 = new Card[5];
+        Card[] card2 = new Card[5];
+        populateFiveCard(card1, card2);
+        System.out.println("Hand One :" + displayCard(card1));
+        System.out.println("Hand two :" + displayCard(card2));
+        System.out.println(CardShufflingAndDealing.dealTwoFivePokerHand(card1, card2));
+    }
 }
