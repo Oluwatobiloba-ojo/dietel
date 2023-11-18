bc085b3ccccde5ee10ab545e25338eddd0346634
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-18 20:49:34 +0100

    commit

--- src/leet_code/UniqueBinaryString.java
+++ src/leet_code/UniqueBinaryString.java
@@ -2,8 +2,7 @@ package leet_code;
 
 public class UniqueBinaryString {
     static int count = 0;
-    public static String unique(String[] nums) {
-        count++;
+    public static String findDifferentBinaryString(String[] nums) {
         boolean result = false;
         String numbs = number(count, nums[0].length());
         for (int counter = 0; counter < nums.length; counter++){
@@ -12,20 +11,27 @@ public class UniqueBinaryString {
             }
             if (counter == nums.length - 1) result = true;
         }
-        if (!result) {return unique(nums);}
+        count++;
+        if (!result) {return findDifferentBinaryString(nums);}
         else {
-            count = 1;
+            count = 0;
             return numbs;
         }
     }
 
     private static String number(int num, int length){
         String result = "";
+        if (num == 0) return padNumberWithZero(length, "1");
         while (num != 0){
             result += num % 2;
             num /= 2;
         }
-        if (result.equals("1")) result = "00";
+        if (result.equals("1")) return padNumberWithZero(length, "0");
+        result = padNumberWithZero(length, result);
+        return result;
+    }
+
+    private static String padNumberWithZero(int length, String result) {
         while (result.length() < length){
             result = "0" + result;
         }
