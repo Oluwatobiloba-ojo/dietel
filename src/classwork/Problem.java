ec4a326cc00207b05bb2d2a82927be4775c4ba69
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-22 19:30:28 +0100

    commit

--- src/classwork/Problem.java
+++ src/classwork/Problem.java
@@ -1,2 +1,25 @@
-package classwork;public class Problem {
+package classwork;
+
+public class Problem {
+    private String name;
+    private Type type;
+    private boolean isSolved;
+
+    public Problem(String name, Type type) {
+        this.name = name;
+        this.type = type;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Type getType() {
+        return type;
+    }
+
+    public String toString(){
+        return String.format("%s, %s", name, type);
+    }
+
 }
