d6836926fd43c2f8964d889b90e1738d4f375a33
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-26 20:18:21 +0100

    commits

--- src/leet_code/ArrayElementNotEqualToNeighbour.java
+++ src/leet_code/ArrayElementNotEqualToNeighbour.java
@@ -8,10 +8,12 @@ public class ArrayElementNotEqualToNeighbour {
     public static int[] notEquals(int[] input) {
         Arrays.sort(input);
         for (int i = 1; i < input.length-1; i++){
-            if (i == (input[i - 1] + input[i + 1]) / 2) {
+            if (input[i] == (input[i - 1] + input[i + 1]) / 2) {
                 int temp = input[input.length - 1];
-                input[input.length - 1] = input[i];
-                input[i] = temp;
+                int second = input[i+1+1];
+                input[input.length - 1] = input[i+1];
+                input[i+1] = second;
+                input[i+1+1] = temp;
             }
         }
         return input;
