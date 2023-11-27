053aae8cb9e62b4dde1223706a7ebff2c1eacae6
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-27 22:14:46 +0100

    commits

--- test/leet_code/MoveElementToEndTest.java
+++ test/leet_code/MoveElementToEndTest.java
@@ -1,6 +1,16 @@
 package leet_code;
 
+import org.junit.jupiter.api.Test;
+
+import static leet_code.MoveElementToEnd.elementToEnd;
 import static org.junit.jupiter.api.Assertions.*;
+
 class MoveElementToEndTest {
-  
+
+    @Test
+    public void testThatElementInAnArrayPickMoveToTheEndOfTheArray(){
+        int[] numbers = {2, 1, 2, 2, 2, 3, 4, 2};
+        int[] result = {1, 3, 4, 2, 2, 2, 2, 2};
+        assertArrayEquals(result, elementToEnd(numbers, 2));
+    }
 }
