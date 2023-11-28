2656fef5ed40dacb20edef40d5d052848f3c5f73
Ojo Oluwatobi <ojot630@gmail.com>
2023-11-28 22:48:16 +0100

    commits

--- test/leet_code/ReverseIntegerTest.java
+++ test/leet_code/ReverseIntegerTest.java
@@ -1,2 +1,26 @@
-package leet_code;public class ReverseIntegerTest {
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReverseIntegerTest {
+
+    @Test
+    public void testForTheReverseOfAnInteger(){
+        int number = 123;
+        assertEquals(321, ReverseInteger.reverse(number));
+    }
+
+    @Test
+    public void testForTheReverseOfAnIntegerWhichIsHavingZeroValueWith(){
+        int number = 120;
+        assertEquals(21, ReverseInteger.reverse(number));
+    }
+
+    @Test
+    public void testForTheReverseOfAnIntegerWithANegativeValue(){
+        int number = -123;
+        assertEquals(-321, ReverseInteger.reverse(number));
+    }
 }
