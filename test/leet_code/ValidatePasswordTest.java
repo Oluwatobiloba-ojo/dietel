818b272ee2648290c80788520942f736273b0a1c
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-04 23:22:46 +0100

    commits

--- test/leet_code/ValidatePasswordTest.java
+++ test/leet_code/ValidatePasswordTest.java
@@ -1,4 +1,23 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ValidatePasswordTest {
-  
+
+    @Test
+    public void testThatAPasswordIsInvalidWhenTheLengthOfPasswordIsBelowEight(){
+        boolean isValid = ValidatePassword.isValid("mathew");
+        assertFalse(isValid);
+    }@Test
+    public void testThatPasswordIsValidWhenTheLengthOfPasswordIsEightAbove(){
+        boolean isValid = ValidatePassword.isValid("oluwatobiloba41");
+        assertTrue(isValid);
+    }@Test
+    public void testThatPasswordOnlyContainLettersAndDigit(){
+        boolean isValid = ValidatePassword.isValid("oluwatobiloba!");
+        assertFalse(isValid);
+    }
+
 }
