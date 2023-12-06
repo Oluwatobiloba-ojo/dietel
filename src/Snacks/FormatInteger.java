efbcca0c11ef874819a831680a21391004869a54
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-06 21:39:34 +0100

    commits

--- src/Snacks/FormatInteger.java
+++ src/Snacks/FormatInteger.java
@@ -1,2 +1,13 @@
-package Snacks;public class FormatInteger {
+package Snacks;
+
+public class FormatInteger {
+    public static String format(int number, int width) {
+        int length = String.valueOf(number).length();
+        String value = String.valueOf(number);
+        while (length < width){
+            value = "0" + value;
+            length = value.length();
+        }
+        return value;
+    }
 }
