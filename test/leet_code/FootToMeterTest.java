a335f3e3e8f7d9c3a747eba5883d383786d1a8b5
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-07 22:53:35 +0100

    first commit

--- test/leet_code/FootToMeterTest.java
+++ test/leet_code/FootToMeterTest.java
@@ -1,4 +1,23 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class FootToMeterTest {
-  
+
+    @Test
+    public void testForTheConversionOfFootToMeter(){
+        assertEquals(0.305, FootToMeter.footToMeter(1));
+    }@Test
+    public void testForTheConversionOf2FeetToMeter(){
+        assertEquals(2.745, FootToMeter.footToMeter(9));
+    }@Test
+    public void testForTheConversionOf10FeetToMeter(){
+        assertEquals(3.050, FootToMeter.footToMeter(10));
+    }@Test
+    public void testForTheConversionOfMeterToFeet(){
+        assertEquals(213.135, FootToMeter.meterToFeet(65));
+    }
+
 }
