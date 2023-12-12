cefd170b41940f31bf0025977ae138a21a390ed7
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-12 19:30:59 +0100

    first commit

--- test/leet_code/LengthOfLastWordTest.java
+++ test/leet_code/LengthOfLastWordTest.java
@@ -1,9 +1,24 @@
 package leet_code;
-
+import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
-
-class PascalTriangleTest {
-
-
-
+class LengthOfLastWordTest {
+    @Test
+    public void testThatTheLengthOfALastWordIs5(){
+        String input = "Hello World";
+        assertEquals(5, LengthOfLastWord.lastWordLength(input));
+    }
+    @Test
+    public void testThatTheLengthOfAWordIs4(){
+        String input = "   fly me to the moon   ";
+        assertEquals(4, LengthOfLastWord.lastWordLength(input));
+    }
+    @Test
+    public void testThatTheLengthOfAWordIs6(){
+        String input = "luffy is still joyboy";
+        assertEquals(6, LengthOfLastWord.lastWordLength(input));
+    }  @Test
+    public void testThatTheLengthOfAWordIs1(){
+        String input = "a";
+        assertEquals(1, LengthOfLastWord.lastWordLength(input));
+    }
 }
