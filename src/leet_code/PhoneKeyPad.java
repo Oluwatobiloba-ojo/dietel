dc64332ccb663b1000267e4457a6e291ada2f376
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-13 23:48:11 +0100

    Phone Key pad enum class

--- src/leet_code/PhoneKeyPad.java
+++ src/leet_code/PhoneKeyPad.java
@@ -1,2 +1,13 @@
-package leet_code;public enum PhoneKeyPad {
+package leet_code;
+
+public enum PhoneKeyPad {
+
+    ABC(2), DEF(3), GHI(4), JKL(5), MNO(6), PQRS(7), TUV(8), WXYV(9);
+    int value;
+    PhoneKeyPad(int value) {
+        this.value = value;
+    }
+    public int getValue(){
+        return value;
+    }
 }
