7e2e2196bd0497fef52f67cad42a7785fe738e65
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-13 23:48:49 +0100

    Phone Key test

--- test/leet_code/TelephoneTest.java
+++ test/leet_code/TelephoneTest.java
@@ -1,4 +1,21 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TelephoneTest {
-  
+
+    @Test
+    public void testThatATelephoneLetterMappingBeConvertToNumberKeypad(){
+        String phoneKeypad = "1-800-Flowers";
+        assertEquals("1-800-3569377", Telephone.getNumber(phoneKeypad));
+    }
+    @Test
+    public void testThatATelephoneLengthMappingConvertToNumberKeyPad(){
+        String phoneKeypad = "1800flowers";
+        assertEquals("18003569377", Telephone.getNumber(phoneKeypad));
+    }
+
+
 }
