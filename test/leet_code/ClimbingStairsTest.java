129d98f104c27d56d3db3208ef57c0d61b62820a
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-14 23:56:28 +0100

    Climbing Stairs Test

--- test/leet_code/ClimbingStairsTest.java
+++ test/leet_code/ClimbingStairsTest.java
@@ -1,4 +1,24 @@
+package leet_code;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class ClimbingStairsTest {
-  
+
+    @Test
+    public void testThatTheyAreTwoWaysToClimbAStepOf2(){
+        int step = 2;
+        assertEquals(2, ClimbingStairs.climbStairs(step));
+    }
+    @Test
+    public void testThatTheyAreThreeWaysToClimbAStepOf3(){
+        int step = 3;
+        assertEquals(3, ClimbingStairs.climbStairs(step));
+    }
+    @Test
+    public void testThatTheyAreFiveWaysToClimbAStairsOf4(){
+        int stairs = 4;
+        assertEquals(5, ClimbingStairs.climbStairs(stairs));
+    }
 }
