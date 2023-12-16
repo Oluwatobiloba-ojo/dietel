f8a119a28f2e83f6cf4a63daac5f6948ed10820c
Ojo Oluwatobi <ojot630@gmail.com>
2023-12-16 23:55:49 +0100

    validate password test

--- test/leet_code/ValidationTest.java
+++ test/leet_code/ValidationTest.java
@@ -2,11 +2,26 @@ package leet_code;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-class IndexOfFirstOccurrenceTest {
+class ValidationTest {
 
     @Test
-    public void testThatWhenTheWeFindTheOccurrenceOfNeedleItRet
+    public void testThatWeValidateAPasswordReturnFalseHavingLengthOfLessThanEight(){
+        String password = "ope";
+        assertFalse(Validation.validate(password));
+    }
+    @Test
+    public void testThatWhenWeValidateAPasswordReturnFalseIfThereIsNoDigitButEight(){
+        String password = "rashidats";
+        assertFalse(Validation.validate(password));
+    }
+    @Test
+    public void testThatWeValidateAPasswordReturnFalseWhenItHavingDigit_LengthOfEightAndNoSpecialChars(){
+        String password = "rashidats12";
+        assertFalse(Validation.validate(password));
+    }
+
 
 }
