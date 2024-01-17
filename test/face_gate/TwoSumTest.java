07233134d5a69eda569512cc614fd871aac7fdb9
Ojo Oluwatobi <ojot630@gmail.com>
2024-01-17 17:28:51 +0100

    face

--- test/face_gate/TwoSumTest.java
+++ test/face_gate/TwoSumTest.java
@@ -1,4 +1,20 @@
+package face_gate;
+
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class TwoSumTest {
-  
+
+    @Test
+    public void testThatTheTwoSumOfNumberBetweenThisArrayIsSevenWithRangeBetweenTwoAndFour(){
+        int[] numbers = {1, 2, 3, 4};
+        assertEquals(7, TwoSum.sumTwoPossibleRangeOfNumber(numbers, 2, 4));
+    }
+    @Test
+    public void testThatTheTheTwoSumOfNumbersBetweenAnArrayIs6WhenTheRangeIsBetweenOneToThree(){
+        int[] numbers = {1, 1, 2, 2, 4, 5};
+        assertEquals(5, TwoSum.sumTwoPossibleRangeOfNumber(numbers, 1, 3));
+    }
+
 }
